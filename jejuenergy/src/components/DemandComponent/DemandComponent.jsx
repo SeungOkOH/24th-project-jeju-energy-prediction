890df92e68c33c2504eae2885ab79daeaa7de5b0
RJihyeon <03x2_18@yonsei.ac.kr>
2024-02-15 22:19:00 +0900

    frontend 완성
    - 백엔드 api 수정만 하면

--- jejuenergy/src/components/DemandComponent/DemandComponent.jsx
+++ jejuenergy/src/components/DemandComponent/DemandComponent.jsx
@@ -10,8 +10,13 @@ function DemandComponent() {
   const [windGen, setWindGen] = useState("Loading");
   const [demandPredictions, setDemandPredictions] = useState("Loading");
   const [dataSets, setDataSets] = useState([
-    { time: "14:20 pm", demand: 100, solarGen: 130, windGen: 150 },
+    { time: "14:00 pm", demand: 100, solarGen: 130, windGen: 150 },
   ]);
+  const [dataSets1, setDataSets1] = useState([
+    { time: "13:00 pm", demand: 100, solarGen: 20, windGen: 30 },
+  ]);
+  const [demandGraph, setDemandGraph] = useState("/solar_output.png");
+  const [loading, setLoading] = useState(true);
 
   // backend에서 데이터 받아오기
   // 조건 : solar + wind가 demand보다 크면
@@ -20,7 +25,7 @@ function DemandComponent() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/api/demand-data");
+        const response = await axios.get("/api/alert-data");
         const newDataSets = response.data.map((item) => ({
           time: item.time,
           demand: item.demand,
@@ -36,14 +41,48 @@ function DemandComponent() {
     fetchData();
   }, []);
 
+  // backend에서 데이터 받아오기
+  // 조건 : solar + wind가 demand보다 작으면
+  // {time, demand, solarGen, windGen} 형태로 보내야함.
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/fuel-data");
+        const newDataSets = response.data.map((item) => ({
+          time: item.time,
+          demand: item.demand,
+          solarGen: item.solarGen,
+          windGen: item.windGen,
+        }));
+        setDataSets1(newDataSets);
+      } catch (error) {
+        // Handle error
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // demand graph 받아오기
+  useEffect(() => {
+    const fetchGraphData = async () => {
+      try {
+        const demandResponse = await axios.get("/demand_graph_url");
+        setDemandGraph(demandResponse.data.image_url);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching graph data:", error);
+      }
+    };
+
+    fetchGraphData();
+  }, []);
+
   return (
     <div className="demand-div">
       <div className="demand-wrapper">
-        <img
-          className="demand-img"
-          src="/solar_output.png"
-          alt="로드에 실패했습니다."
-        />
+        <img className="demand-img" src={demandGraph} alt="Demand Graph" />
       </div>
 
       <div className="alert-div">
@@ -76,6 +115,28 @@ function DemandComponent() {
           FOSSIL FUEL GEN PROPOSAL
           <MdOutlineLocalFireDepartment size="24" color="C8A190" />
         </h1>
+        {dataSets1.map((item) => {
+          if (item.demand > item.solarGen + item.windGen) {
+            return (
+              <div key={item.time} className="data-set">
+                <h3>{item.time}</h3>
+                <h4>
+                  DEMAND <br /> {item.demand}
+                </h4>
+                <h4>
+                  SOLAR <br /> {item.solarGen}
+                </h4>
+                <h4>
+                  WIND <br /> {item.windGen}
+                </h4>
+                <h5 className="proposal">
+                  PROPOSAL <br /> {item.demand - item.solarGen - item.windGen}
+                </h5>
+              </div>
+            );
+          }
+          return null;
+        })}
       </div>
     </div>
   );
--- jejuenergy/src/components/GenComponent/GenComponent.jsx
+++ jejuenergy/src/components/GenComponent/GenComponent.jsx
@@ -1,24 +1,39 @@
 import "./GenComponent.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function GenComponent() {
+  const [solarGraph, setSolarGraph] = useState("/solar_output.png");
+  const [windGraph, setWindGraph] = useState("/solar_output.png");
+  const [loading, setLoading] = useState(true);
+
+  //backend에서 데이터 받아오기 (그래프 시각화하나 url을 보내줘야함 Django)
+  useEffect(() => {
+    const fetchGraphData = async () => {
+      try {
+        const solarResponse = await axios.get("/solar_graph_url");
+        const windResponse = await axios.get("/wind_graph_url");
+        setSolarGraph(solarResponse.data.image_url);
+        setWindGraph(windResponse.data.image_url);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching graph data:", error);
+      }
+    };
+
+    fetchGraphData();
+  }, []);
+
   return (
     <div className="gen-div">
       <div className="gen-welcome">
         <p className="p-welcome">WELCOME TO JEJU!</p>
       </div>
       <div className="gen-graph">
-        <img
-          className="gen-img"
-          src="/solar_output.png"
-          alt="로드에 실패했습니다."
-        />
+        <img className="gen-img" src={solarGraph} alt="Solar Graph" />
       </div>
       <div className="gen-graph">
-        <img
-          className="gen-img"
-          src="/solar_output.png"
-          alt="로드에 실패했습니다."
-        />
+        <img className="gen-img" src={windGraph} alt="Wind Graph" />
       </div>
     </div>
   );
