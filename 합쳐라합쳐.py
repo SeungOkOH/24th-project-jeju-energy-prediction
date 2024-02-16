229e3d9084cd8d7c4965a685ac552dfb7acb6329
yeeyee <2019147559@yonsei.ac.kr>
2024-02-16 20:05:49 +0900

    합쳐보자

---
+++ 합쳐라합쳐.py
@@ -0,0 +1,12 @@
+import tommorow_data_get
+import torch
+
+data = tommorow_data_get.get_data()
+
+elec_model_path = '/models/elec_model.pt'
+solar_model_path = '/models/solar_model.pt'
+wind_model_path = '/models/wind_model.pt'
+
+elec_model = torch.load(elec_model_path)
+solar_model = torch.load(solar_model_path)
+wind_model = torch.load(wind_model_path)
